4e4c8776d4d7cca77e71c5fb06e44dee075eab01
cmotc <cmotc@openmailbox.org>
2015-04-29 15:43:48 -0400

    restarted library project

---
+++ libtox_net/src/libtox_net.h
@@ -0,0 +1,21 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
+/*
+ * lib.h
+ * Copyright (C) 2015 cmotc <cmotc@openmailbox.org>
+ * 
+ * libtox_net is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * libtox_net is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.";
+ */
+
+int libtox_net_func (void);
+
